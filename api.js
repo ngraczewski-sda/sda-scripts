c93ed9bab4af3e6ef54b688f11bc81f76f83d3a0
Nikodem Graczewski <ngraczewski.sda@gmail.com>
2022-05-18 01:51:50 +0200

    updated api.js to accept a callback and return a promise

--- api.js
+++ api.js
@@ -1,15 +1,41 @@
 var studentId;
 
-studentId =
-  studentId ?? localStorage.getItem("studentId") ?? `${Math.random()}`;
-localStorage.setItem("studentId", studentId);
+const submitSolution = (() => {
+  const URL = "https://sda-solutions.herokuapp.com?";
+  const getStudentId = () => {
+    studentId =
+      studentId ?? localStorage.getItem("studentId") ?? `${Math.random()}`;
 
-const sendSolution = (solution) => {
-  fetch(`https://sda-solutions.herokuapp.com?id=${studentId}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ solution }),
-  });
-};
+    localStorage.setItem("studentId", studentId);
+
+    return studentId;
+  };
+
+  const getArguments = async () => {
+    const response = await fetch(`${URL}/arguments`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return await response.json();
+  };
+
+  const sendSolution = async (solution) => {
+    const response = await fetch(`${URL}/responses?id=${getStudentId()}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ solution }),
+    });
+
+    return await response.json();
+  };
+
+  return async (solutionCallback) => {
+    const args = await getArguments();
+    return sendSolution(solutionCallback(...args));
+  };
+})();
